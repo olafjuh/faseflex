a0ec1a8bdc7c8eb8734595a6813503de55e1bdec
olafjuh <stargatesg1>
2015-12-06 14:00:26 +0100

    dbs+ctx+wishlistcontroller

---
+++ ihff/Controllers/DbRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ihff.Models;
+
+namespace ihff.Controllers
+{
+    public class DbRepository
+    {
+        private ihffContext ctx = new ihffContext();
+
+
+        public IEnumerable<Wishlist> GetAllWishlists()
+        {
+            return ctx.wishlists;
+        }
+        
+
+        //save wishlist
+
+        //delete wishlist
+
+        //add activity to wishlist
+        
+        //remove activity to wishlist
+
+        //edit activity in wishlist
+    }
+}
---
+++ ihff/Controllers/ihffContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ihff.Controllers
+{
+    public class ihffContext : DbContext
+    {
+         public ihffContext()
+            : base("ihffTestConnection")
+        {
+            
+        }
+
+        public DbSet<ihff.Models.Activiteit> activiteiten { get; set; }
+        public DbSet<ihff.Models.WishlistItem> wishlistItems { get; set; }
+        public DbSet<ihff.Models.Wishlist> wishlists { get; set; }
+    }
+}
