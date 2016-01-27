639d04f8ad9749d2f438e6857f73df1e1bc9ac49
Martin <junocaen@hotmail.com>
2016-01-27 19:32:10 +0100

    moartin startje

--- ihff/Models/Activitieswednesday.cs
+++ ihff/Models/Activitieswednesday.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace ihff.Models
 {
     public class Activitywednesday
