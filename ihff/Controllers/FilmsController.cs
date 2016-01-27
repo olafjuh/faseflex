8ffd9f650977b4083f74c8a2a991e6c8622dcef8
olafjuh <stargatesg1>
2016-01-27 12:50:45 +0100

    added

---
+++ ihff/Controllers/FilmsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ihff.Models;
+
+namespace ihff.Controllers
+{
+    public class FilmsController : Controller
+    {
+        private DbRepository repository = new DbRepository();
+
+        public ActionResult Index()
+        {
+            return View(repository.GetAllFilms());
+        }
+
+        public ActionResult AddItem(int id)
+        {
+            Activity activity = repository.GetFilm(id);
+            WishlistItem wishlistItem = new WishlistItem();
+            
+            wishlistItem.name = activity.name;
+            wishlistItem.location = activity.location;
+            wishlistItem.startTime = activity.startTime;
+            wishlistItem.endTime = activity.endTime;
+            wishlistItem.actID = activity.Id;
+
+            wishlistItem.type = "Film";
+            return View(wishlistItem);
+            
+        }
+
+        [HttpPost]
+        public ActionResult AddItem(WishlistItem wishlistItem)
+        {
+            Wishlist wishlist = new Wishlist();
+
+            if (!(Session["active_wishlist"] == null))
+            {
+                wishlist = (Wishlist)Session["active_wishlist"];
+                wishlist.wishlistItems.Add(wishlistItem);
+            }
+            else
+            {
+                wishlist.wishlistItems.Add(wishlistItem);
+                Session["active_wishlist"] = wishlist;
+            }
+            
+            return View();
+        }
+        // check
+    }
+}
